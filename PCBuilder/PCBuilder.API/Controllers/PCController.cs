ee9c04272da5b66acebdb98a0835649c16c0fba0
khacodee <114900252+khacodee@users.noreply.github.com>
2023-06-11 10:53:24 +0700

    CRUD
    CRUD

--- PCBuilder/PCBuilder.API/Controllers/PCController.cs
+++ PCBuilder/PCBuilder.API/Controllers/PCController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PCBuilder.Repository.Models;
+using PCBuilder.Services.DTO;
 using PCBuilder.Services.Service;
 
 namespace PCBuilder.API.Controllers
@@ -20,5 +21,56 @@ namespace PCBuilder.API.Controllers
             var PCs = await _IPCServices.GetPCList();
             return Ok(PCs);
         }
+        [HttpGet("{PcId}")]
+        public async Task<IActionResult> GetPcByIdList(int PcId)
+        {
+            var pc = await _IPCServices.GetPCByID(PcId);
+
+            if (pc == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pc);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreatePC([FromBody] PcDTO pcCreateDTO)
+        {
+            var response = await _IPCServices.CreatePC(pcCreateDTO);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePC(int id, [FromBody] PcDTO pcUpdateDTO)
+        {
+            var response = await _IPCServices.UpdatePC(id, pcUpdateDTO);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePC(int id)
+        {
+            var response = await _IPCServices.DeletePC(id);
+
+            if (!response.Success)
+            {
+                return NotFound(response);
+            }
+
+            return Ok(response);
+        }
+
     }
 }
--- PCBuilder/PCBuilder.Repository/Repository/UserRepository.cs
+++ PCBuilder/PCBuilder.Repository/Repository/UserRepository.cs
@@ -16,7 +16,9 @@ namespace PCBuilder.Repository.Repository
         /// </summary>
         /// <returns>Models.User</returns>
         Task<ICollection<User>> GetAllUsersAsync();
-
+        Task<User> CreateUserAsync(User user);
+        Task<User> UpdateUserAsync(User user);
+        Task<bool> DeleteUserAsync(int id);
 
 
     }
@@ -31,6 +33,38 @@ namespace PCBuilder.Repository.Repository
         public async Task<ICollection<User>> GetAllUsersAsync()
         {
             return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> CreateUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User> UpdateUserAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<bool> DeleteUserAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return false;
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return true;
         }
     }
 }
