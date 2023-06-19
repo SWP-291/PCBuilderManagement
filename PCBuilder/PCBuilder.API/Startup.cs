814cb5436359c0a1cf328836262d97a481ecb2f0
khacodee <114900252+khacodee@users.noreply.github.com>
2023-06-19 17:56:37 +0700

    edit Startup

--- PCBuilder/PCBuilder.API/Startup.cs
+++ PCBuilder/PCBuilder.API/Startup.cs
@@ -79,7 +79,7 @@ namespace PCBuilder.API
                 app.UseHsts();
             }
             app.UseCors(builder =>
-                builder.WithOrigins("http://localhost:3000")
+                builder.WithOrigins("https://localhost:3000")
                        .AllowAnyHeader()
                        .AllowAnyMethod());
 
